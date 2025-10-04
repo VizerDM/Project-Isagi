b202bcf0e54bc009f226e052ba37db8421715667
VizerDM <simo.elyou105@gmail.com>
2025-10-05 00:26:04 +0100

    Finished Interactivity

--- src/Tracker.tsx
+++ src/Tracker.tsx
@@ -4,38 +4,38 @@ type Habit = {
   name: string;
   checks: boolean[];
 };
-const [habits, SetHabits] = useState<Habit[]>([
-  { name: "GYM", checks: Array(30).fill(false) },
-]);
-
-const onCheck = (habitIndex: number, dayIndex: number): void => {
-  // handler for when a checkbox is ticked/unticked
-
-  SetHabits(
-    (
-      prevHabits //The useState Modifier, this is the second method that uses the previous object
-    ) =>
-      prevHabits.map(
-        (
-          habit,
-          hIdx //copying each habit in a new array of habits (new state ig)
-        ) =>
-          habitIndex === hIdx
-            ? {
-                ...habit, //Spread Op. it copies all propreties of the previous habit obj in this the name.
-                checks: habit.checks.map(
-                  (
-                    checked,
-                    dIdx //now we copy the checks array and change only the checked day
-                  ) => (dIdx === dayIndex ? !checked : checked)
-                ),
-              }
-            : habit
-      )
-  );
-};
 
 function Tracker() {
+  const [habits, SetHabits] = useState<Habit[]>([
+    { name: "GYM", checks: Array(30).fill(false) },
+  ]);
+
+  const onCheck = (habitIndex: number, dayIndex: number): void => {
+    // handler for when a checkbox is ticked/unticked
+    SetHabits(
+      (
+        prevHabits //The useState Modifier, this is the second method that uses the previous object
+      ) =>
+        prevHabits.map(
+          (
+            habit,
+            hIdx //copying each habit in a new array of habits (new state ig)
+          ) =>
+            habitIndex === hIdx
+              ? {
+                  ...habit, //Spread Op. it copies all propreties of the previous habit obj in this the name.
+                  checks: habit.checks.map(
+                    (
+                      checked,
+                      dIdx //now we copy the checks array and change only the checked day
+                    ) => (dIdx === dayIndex ? !checked : checked)
+                  ),
+                }
+              : habit
+        )
+    );
+  };
+
   const days = Array.from({ length: 30 }, (_, i) => i + 1);
 
   return (
@@ -55,7 +55,7 @@ function Tracker() {
                 <input
                   type="checkbox"
                   checked={habits[0].checks[day - 1]}
-                  onchange={onCheck(day, 0)}
+                  onChange={() => onCheck(0, day - 1)}
                 ></input>
               </td>
             ))}
