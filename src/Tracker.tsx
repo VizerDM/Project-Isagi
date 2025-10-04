89056241ac58968274a988f2a9e694df1fe028a7
VizerDM <simo.elyou105@gmail.com>
2025-10-05 00:12:09 +0100

    Working on Interactivity OnCheck func

--- src/Tracker.tsx
+++ src/Tracker.tsx
@@ -8,7 +8,32 @@ const [habits, SetHabits] = useState<Habit[]>([
   { name: "GYM", checks: Array(30).fill(false) },
 ]);
 
-const [checks, SetChecks] = useState<boolean[]>(Array(30).fill(false));
+const onCheck = (habitIndex: number, dayIndex: number): void => {
+  // handler for when a checkbox is ticked/unticked
+
+  SetHabits(
+    (
+      prevHabits //The useState Modifier, this is the second method that uses the previous object
+    ) =>
+      prevHabits.map(
+        (
+          habit,
+          hIdx //copying each habit in a new array of habits (new state ig)
+        ) =>
+          habitIndex === hIdx
+            ? {
+                ...habit, //Spread Op. it copies all propreties of the previous habit obj in this the name.
+                checks: habit.checks.map(
+                  (
+                    checked,
+                    dIdx //now we copy the checks array and change only the checked day
+                  ) => (dIdx === dayIndex ? !checked : checked)
+                ),
+              }
+            : habit
+      )
+  );
+};
 
 function Tracker() {
   const days = Array.from({ length: 30 }, (_, i) => i + 1);
@@ -30,6 +55,7 @@ function Tracker() {
                 <input
                   type="checkbox"
                   checked={habits[0].checks[day - 1]}
+                  onchange={onCheck(day, 0)}
                 ></input>
               </td>
             ))}
